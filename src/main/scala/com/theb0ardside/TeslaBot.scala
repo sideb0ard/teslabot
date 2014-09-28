4c4e974a82ff496a739f1b6c31e010561251c7c7
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2014-09-27 20:09:58 -0700

    first commit

---
+++ src/main/scala/com/theb0ardside/TeslaBot.scala
@@ -0,0 +1,85 @@
+package com.theb0ardside
+
+import akka.actor.{ Actor, ActorLogging, ActorSystem, ActorRef, Props, Terminated }
+import akka.io.{ IO, Tcp }
+import akka.util.{ByteString, Timeout}
+import akka.pattern.ask
+import java.net.InetSocketAddress
+import java.util.concurrent.TimeUnit
+import scala.io.StdIn
+import scala.collection.immutable.StringOps
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+case class Message(msg: String)
+
+object TeslaBotServiceApp extends App {
+  val system = ActorSystem("wakka-service-system")
+  val endpoint = new InetSocketAddress("localhost", 8088)
+  system.actorOf(TeslaBotService.props(endpoint), "wakka-service")
+  StdIn.readLine(f"Hit ENTER to exit ...\n")
+  system.shutdown()
+}
+
+object TeslaBotService {
+  def props(endpoint: InetSocketAddress): Props = 
+    Props(new TeslaBotService(endpoint))
+}
+
+class TeslaBotService(endpoint: InetSocketAddress) extends Actor with ActorLogging {
+  import context.system
+
+  val name = "TESLABOT"
+
+  IO(Tcp) ! Tcp.Bind(self, endpoint)
+
+  override def receive: Receive = {
+    case Tcp.Connected(remote, _) =>
+      log.debug("Remote Address {} connected", remote)
+      sender ! Tcp.Register(context.actorOf(TeslaBotConnectionHandler.props(remote, sender, name)))
+      sender ! Tcp.Write(ByteString.apply("I'M " + name + ". WHO THE FUCK ARE YOU?\n"))
+  }
+}
+
+object LanguageProcessor {
+  def props(text: String): Props = 
+    Props(new LanguageProcessor(text))
+}
+
+class LanguageProcessor(text: String) extends Actor with ActorLogging {
+  def receive: Receive = {
+    case Message(msg) => 
+      println("YEH, BOT, GOT ME SOME " + msg)
+      sender ! "FUCK YEH, " + msg.toUpperCase + "!!\n"
+  }
+}
+
+object TeslaBotConnectionHandler {
+  def props(remote: InetSocketAddress, connection: ActorRef, name: String): Props =
+    Props(new TeslaBotConnectionHandler(remote, connection, name))
+}
+
+class TeslaBotConnectionHandler(remote: InetSocketAddress, connection: ActorRef, name: String) extends Actor with ActorLogging {
+  context.watch(connection)
+
+  val lp = context.actorOf(LanguageProcessor.props("blh"))
+  implicit val timeout = Timeout(1 second)
+
+  def receive: Receive = {
+    case Tcp.Received(data) =>
+      val text = data.utf8String.trim
+      log.debug("Received {} from remote address {}", text, remote)
+      text match {
+        case "close" => context.stop(self)
+        case _ => sender ! Tcp.Write(
+          ByteString.apply(
+            Await.result(lp ? Message(text), timeout.duration).asInstanceOf[String]))
+      }
+    case _: Tcp.ConnectionClosed =>
+      log.debug("Stopping, because remote address {} closed", remote)
+      context.stop(self)
+    case Terminated(`connection`) =>
+      log.debug("Stopping, remote {} died", remote)
+  }
+}
---
+++ src/main/scala/com/theb0ardside/echy.scala
@@ -0,0 +1,18 @@
+
+/*
+*sealed trait Event
+*events
+*class ChatClient(val name: String) {
+*class Session(user: String, storage: ActorRef) extends Actor {
+*trait ChatStorage extends Actor
+*class MemoryChatStorage extends ChatStorage {
+*trait SessionManagement { this: Actor =>
+*trait ChatManagement { this: Actor =>
+*trait MemoryChatStorageFactory { this: Actor =>
+*trait ChatServer extends Actor {
+*class ChatService extends
+*object ServerRunner {
+*object ClientRunner {
+*
+*/
+
