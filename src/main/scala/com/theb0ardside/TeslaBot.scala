fc0bd4fae6d49834843658a7f5aaeda76e1fe386
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2014-09-28 17:25:55 -0700

    Name calling

--- src/main/scala/com/theb0ardside/TeslaBot.scala
+++ src/main/scala/com/theb0ardside/TeslaBot.scala
@@ -13,46 +13,74 @@ import scala.concurrent.duration._
 import scala.language.postfixOps
 
 case class Message(msg: String)
+case object AskNameMessage
 
-object TeslaBot extends App {
+object TeslaBotMain extends App {
   val system = ActorSystem("teslabot-system")
   val endpoint = new InetSocketAddress("localhost", 8088)
-  system.actorOf(TeslaBotService.props(endpoint), "teslabot-service")
+  system.actorOf(TeslaBot.props(endpoint), "teslabot-service")
   StdIn.readLine(f"Hit ENTER to exit ...\n")
   system.shutdown()
 }
 
-object TeslaBotService {
+object TeslaBot {
+  val name = "::TESLA::BOT::"
+  var peopleIKnow = List[Person]()
   def props(endpoint: InetSocketAddress): Props = 
-    Props(new TeslaBotService(endpoint))
+    Props(new TeslaBot(endpoint))
 }
 
-class TeslaBotService(endpoint: InetSocketAddress) extends Actor with ActorLogging {
+class TeslaBot(endpoint: InetSocketAddress) extends Actor with ActorLogging {
   import context.system
   IO(Tcp) ! Tcp.Bind(self, endpoint)
-
-  val name = "::TESLA::BOT::"
 
   override def receive: Receive = {
     case Tcp.Connected(remote, _) =>
       log.debug("Remote Address {} connected", remote)
-      sender ! Tcp.Register(context.actorOf(TeslaBotConnectionHandler.props(remote, sender, name)))
-      sender ! Tcp.Write(ByteString.apply("I'M " + name + ". WHO THE FUCK ARE YOU?\n"))
+      val percy = new Person
+      sender ! Tcp.Register(context.actorOf(TeslaBotConnectionHandler.props(remote, sender)))
   }
 }
 
 object TeslaBotConnectionHandler {
-  def props(remote: InetSocketAddress, connection: ActorRef, name: String): Props =
-    Props(new TeslaBotConnectionHandler(remote, connection, name))
+  def props(remote: InetSocketAddress, connection: ActorRef): Props =
+    Props(new TeslaBotConnectionHandler(remote, connection))
 }
 
-class TeslaBotConnectionHandler(remote: InetSocketAddress, connection: ActorRef, name: String) extends Actor with ActorLogging {
+class TeslaBotConnectionHandler(remote: InetSocketAddress, connection: ActorRef) extends Actor with ActorLogging {
   context.watch(connection)
 
   val lp = context.actorOf(Props[LanguageProcessor])
-  implicit val timeout = Timeout(1 second)
+  implicit val timeout = Timeout(10 seconds)
+
+  val p = new Person
+  println("PNAME: " + p.getName)
+
+  if (p.getName == "") {
+    connection ! Tcp.Write(ByteString.apply("I'M " + TeslaBot.name + ". WHO THE FUCK ARE YOU?\n"))
+  }
 
   def receive: Receive = {
+    case Tcp.Received(data) =>
+      val name = data.utf8String.trim
+      log.debug("Received {} from remote address {}", name, remote)
+      name match {
+        case "close" => context.stop(self)
+        case _ => { 
+          println("NAME IS " + name)
+          p.setName(name)
+          sender ! Tcp.Write(ByteString.apply("PLEASED TO MEET YOU, " + p.getName.toUpperCase + ". WHAT'D YA WANNA YAMMER ABOUT?\n"))
+          context.become(converse)
+        }
+      }
+    case _: Tcp.ConnectionClosed =>
+      log.debug("Stopping, because remote address {} closed", remote)
+      context.stop(self)
+    case Terminated(`connection`) =>
+      log.debug("Stopping, remote {} died", remote)
+  }
+
+  def converse: Receive = {
     case Tcp.Received(data) =>
       val text = data.utf8String.trim
       log.debug("Received {} from remote address {}", text, remote)
@@ -60,7 +88,7 @@ class TeslaBotConnectionHandler(remote: InetSocketAddress, connection: ActorRef,
         case "close" => context.stop(self)
         case _ => sender ! Tcp.Write(
           ByteString.apply(
-            Await.result(lp ? Message(text), timeout.duration).asInstanceOf[String]))
+              Await.result(lp ? Message(text), timeout.duration).asInstanceOf[String]))
       }
     case _: Tcp.ConnectionClosed =>
       log.debug("Stopping, because remote address {} closed", remote)
@@ -78,3 +106,8 @@ class LanguageProcessor extends Actor with ActorLogging {
   }
 }
 
+class Person {
+  private var name: String = ""
+  def setName(n: String) { name = n }
+  def getName() = name 
+}
